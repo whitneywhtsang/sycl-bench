2c29b8a93f85698fb7301d76b837396d8d67656c
Tsang, Whitney <whitney.tsang@intel.com>
2023-02-15 22:10:00 -0800

    Add performance results
    Signed-off-by: Tsang, Whitney <whitney.tsang@intel.com>

---
+++ build_cgeist/CMakeFiles/covariance.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/covariance.dir/polybench/covariance.cpp.o"
+  "covariance"
+  "covariance.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/covariance.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
