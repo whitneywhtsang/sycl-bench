d05e5889e53aae1af6fac57a80bc9ba1f3a9ae9a
Lukas Sommer <lukas.sommer@codeplay.com>
2023-08-15 16:04:08 +0200

    [SYCL-MLIR] Python scripts for data visualization (#8)
    Adds a Python script to retrieve relevant data from the CSV output file
    of SYCL Bench runs and two scripts to visualize the data in plots.
    
    ---------
    
    Signed-off-by: Lukas Sommer <lukas.sommer@codeplay.com>

---
+++ plot/README.md
@@ -0,0 +1,162 @@
+# Visualization
+
+This folder contains scripts to process and visualize performance data from 
+the SYCL Bench benchmark suite.
+
+## Setup
+
+Prior to using the scripts, you must setup a suitable Python environment.
+
+If you do not want to install the Python packages system-wide, it is 
+recommended to use a Python virtual environment instead. 
+
+First, you need to install Python `venv` on your system. On Ubuntu, this 
+is possible through `apt install python3-venv`. If you use a different system,
+check instructions for your system online. 
+
+After that, a new Python virtual environment can be created:
+```bash
+python3 -m venv venv
+```
+
+The second argument is the name of the folder that you would like to use for
+the virtual environment, so you're free to choose a different name. If you do 
+so, remember to use the correct name in the following commands.
+
+Activate the virtual environment:
+```bash
+source venv/bin/activate
+```
+
+This command needs to be repeated every time you start a new shell environment.
+
+
+To install the necessary packages, run: 
+```bash
+pip install pandas seaborn matplotlib
+```
+
+This step only needs to performed once during setup. 
+
+
+## Preprocessing
+
+SYCL Bench can provide raw runtime data in CSV format. However, this format is
+not directly suitable for formatting and therefore needs to undergo
+pre-processing. 
+
+The input format is expected to start with two lines with a single column
+giving a name for the current benchmark run (first line) and the configuration
+(second line).
+
+This is followed by alternating lines of header and data. The header is
+expected to define the following fields, for which the data line gives values:
+
+* `# Benchmark name`: Name of the benchmark
+* `sycl-implementation`: Name for the SYCL implementation
+* `device-name`: Name of the device the benchmark was executed on
+* `problem-size`: Problem size given for the benchmark
+* `local-size`: SYCL work-group size
+* `kernel-time-samples` (Optional): List of kernel time samples
+* `run-time-samples`: List of run time samples
+
+The last two fields are expected to encode the list of samples as an
+(optionally quoted) string, separating the different samples with a
+whitespace character.
+
+An example input file with two benchmarks might look like this:
+```
+some-name
+some-config
+# Benchmark name, problem-size, local-size, kernel-time-samples, run-time-samples, device-name, sycl-implementation
+GEMM, 4096, 256, 5 6 5 7, "10 11 12 9", "Fast GPU", "popSYCL"
+# Benchmark name, problem-size, local-size, kernel-time-samples, run-time-samples, device-name, sycl-implementation
+SPMV, 8192, 128, 0.5 0.6 0.55, 0.8 0.9 1.1, "Fast GPU", "popSYCL"
+```
+
+The pre-processing script parses this format and creates a Pandas dataframe 
+from it, suitable for plotting, which is also stored in CSV format.
+
+The script can be executed as follows:
+
+```bash
+python src/parse-csv.py input.csv -o dataframe.csv
+```
+
+## Detailed visualization
+
+The first visualization script will create one plot *per* benchmark,
+allowing detailed investigation of different configurations (problem size, 
+local size) of that benchmark. 
+
+The script can be executed as follows and will yield a single PDF file with
+one page per benchmark: 
+
+```bash
+python src/plot-detailed.py fast.csv slow.csv -o detailed.pdf
+```
+
+If you want to investigate kernel time from profiling rather than runtime, you
+can do so by specifying the `--kernel-time` flag.
+
+
+## Compact visualization
+
+The second visualization script will create a single plot for more compact
+visualization, e.g., for inclusion in a publication.
+
+The visualization can be configured through a configuration file. The
+configuration file supports the following fields:
+
+* `alias`: An object, where each field defines an alias for a benchmark or
+a name. 
+* `benchmarks`: A list of objects. Each object must specify the `name` of a
+benchmark and can optionally include a field `configs` with an array of 
+configurations specified through an object with fields `problem-size` and
+`local-size`. Only benchmarks/configurations included in this list will be 
+plotted. For the `name` field, you can use the full name of the benchmark or
+the corresponding alias you defined. 
+
+An example configuration might look like this:
+
+```json
+{
+    "alias": {
+        "mlir" : "SYCL-MLIR",
+        "spir" : "DPC++",
+        "Polybench_2DConvolution": "2D Convolution",
+        "Polybench_Syrk": "SYRK",
+    },
+    "benchmarks": [
+        {
+            "name" : "SYRK"
+        },
+        {
+            "name": "Polybench_3mm",
+            "configs": [
+                {
+                    "problem-size": 1024,
+                    "local-size": 256
+                }
+            ]
+        }
+    ]
+}
+```
+
+The script also takes two mandatory flags `--numerator` (`-n`) and
+`--denominator` (`-d`), which take the name of the numerator and denominator
+that should be used in the speedup calculation.
+
+The visualization script can be executed as follows and will yield a PDF file:
+```bash
+python src/plot-compact.py fast.csv slow.csv -o compact.pdf -c config.json -n "slow-compiler" -d "fast-compiler"
+```
+
+The visualization can be customized further using the following options:
+* `-t`/`--title`: Set a custom title for the plot
+* `-l`/`--ylabel`: Set a custom label for the y-axis
+* `-y`/`--ylim`: Set a custom maximum value for the y-axis (`float`)
+
+Again, if you want to investigate kernel time from profiling rather than
+runtime, you can do so by specifying the `--kernel-time` flag.
---
+++ plot/plot-compact.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+import argparse
+import json
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from scipy.stats import gmean
+
+
+def getBenchConfig(config, benchName, problemSize, localSize):
+    aliasName = config.translate(benchName) if config else benchName
+    return f"{aliasName} ({problemSize}x{localSize})"
+
+
+class BenchConfig:
+    prefix = None
+    allowedConfigs = []
+
+    def __init__(self, config, benchName, allowedConfigList):
+        if allowedConfigList:
+            self.allowedConfigs = [
+                getBenchConfig(config, benchName, x[0], x[1]) for x in allowedConfigList
+            ]
+        else:
+            self.prefix = config.translate(benchName) if config else benchName
+
+    def matches(self, benchConfig):
+        if self.allowedConfigs:
+            return benchConfig in self.allowedConfigs
+        else:
+            return benchConfig.startswith(self.prefix)
+
+    def __str__(self):
+        if self.allowedConfigs:
+            return ", ".join(self.allowedConfigs)
+        else:
+            return self.prefix
+
+
+# end of class BenchConfig
+
+
+class PlotConfig:
+    aliases = None
+    plotBenches = []
+
+    def __init__(self, definedAliases):
+        self.aliases = definedAliases
+
+    def translate(self, benchName):
+        return self.aliases.get(benchName, benchName) if self.aliases else benchName
+
+    def add_bench(self, benchConfig):
+        self.plotBenches.append(benchConfig)
+
+    def should_plot(self, benchConfigStr):
+        return any([x.matches(benchConfigStr) for x in self.plotBenches])
+
+    def __str__(self):
+        return f"aliases: {self.aliases}, benchmarks:\n {[str(b) for b in self.plotBenches]}"
+
+
+# end of class PlotConfig
+
+
+def parsePlotConfig(configFile):
+    with open(configFile) as f:
+        data = json.load(f)
+        aliases = None
+        if "alias" in data:
+            aliases = data["alias"]
+
+        config = PlotConfig(aliases)
+
+        if "benchmarks" in data:
+            for b in data["benchmarks"]:
+                if "name" in b:
+                    benchName = b["name"]
+                    configs = []
+                    if "configs" in b:
+                        for c in b["configs"]:
+                            if ("problem-size" in c) and ("local-size") in c:
+                                configs.append((c["problem-size"], c["local-size"]))
+
+                    config.add_bench(BenchConfig(config, benchName, configs))
+
+        return config
+
+
+def column_name(useKernelTime):
+    return "Kernel" if useKernelTime else "Runtime"
+
+
+def filter_data(df, useKernelTime):
+    columnName = column_name(useKernelTime)
+    return df[df[columnName].notnull()]
+
+
+def calculate_speedup(df, bench, numerator, denominator, useKernelTime):
+    return (
+        df[((df["Bench"] == bench) & (df["Name"] == numerator))][
+            column_name(useKernelTime)
+        ].mean()
+        / df[((df["Bench"] == bench) & (df["Name"] == denominator))][
+            column_name(useKernelTime)
+        ].mean()
+    )
+
+
+def preprare_data(df, config, useKernelTime, numerator, denominator):
+    # Create a new column combining benchmark and configuration
+    df["BenchConfig"] = df.apply(
+        lambda x: getBenchConfig(
+            config, x["Bench"], x["Problem Size"], x["Local Size"]
+        ),
+        axis=1,
+    )
+
+    # Filter to only include the benchmarks we actually want to plot.
+    df = df[df["BenchConfig"].apply(lambda b: config.should_plot(b))]
+
+    if config:
+        df.loc[:, "Bench"] = df["Bench"].apply(config.translate)
+
+    speedups = [
+        [bench, calculate_speedup(df, bench, numerator, denominator, useKernelTime)]
+        for bench in df["Bench"].unique()
+    ]
+
+    return pd.DataFrame(speedups, columns=["Bench", "Speedup"])
+
+
+def plot_external(
+    inputFiles,
+    outputFile,
+    useKernelTime,
+    configFile,
+    numerator,
+    denominator,
+    title,
+    ylim,
+    ylabel
+):
+    sns.set_style("whitegrid")
+    df = None
+    # Read all files and merge to one dataframe
+    for f in inputFiles:
+        if df is None:
+            df = pd.read_csv(f, header=0)
+        else:
+            df = pd.concat([df, pd.read_csv(f, header=0)], ignore_index=True)
+
+    # Parse the configuration file if given.
+    config = parsePlotConfig(configFile) if configFile else None
+
+    num = numerator
+    denom = denominator
+    if config:
+        df["Name"] = df["Name"].apply(lambda x: config.translate(x))
+        num = config.translate(num)
+        denom = config.translate(denom)
+
+    # Filter only the entries for the time we currently want to plot.
+    # Either kernel time or runtime.
+    df = filter_data(df, useKernelTime)
+
+    # Add calculated columns and filter out benchmarks and configurations that
+    # should not be plotted
+    data = preprare_data(df, config, useKernelTime, num, denom)
+
+
+    order = list(data["Bench"].unique())
+    order.sort()
+    order.append("geo.-mean")
+
+    geomean = gmean(data["Speedup"])
+
+    geomeanRow = pd.DataFrame([["geo.-mean", geomean]], columns=["Bench", "Speedup"])
+
+    data = pd.concat([data, geomeanRow], ignore_index=True)
+
+    with PdfPages(outputFile) as pdf:
+        fig = plt.figure()
+        plt.xticks(rotation=85)
+        # Set plot title
+        if title:
+            print(str(title))
+            plt.title(str(title))
+        # Barplot
+        ax = sns.barplot(
+            data, x="Bench", y="Speedup", color=sns.color_palette()[0], order=order
+        )
+        ax.bar_label(ax.containers[0], fmt="%.2fx", rotation=80)
+        # Set axes labels
+        ax.set(xlabel=None, ylabel=ylabel)
+        # Set y-axis limit
+        if ylim:
+            ax.set_ylim(top=float(ylim))
+
+        pdf.savefig(fig, bbox_inches="tight")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create one plot for all benchmarks")
+    parser.add_argument("inputs", nargs="+", default=[])
+    parser.add_argument("-o", "--output", default="external.pdf")
+    parser.add_argument("--kernel-time", action="store_true")
+    parser.add_argument("-c", "--config", default=None)
+    parser.add_argument("-n", "--numerator", required=True)
+    parser.add_argument("-d", "--denominator", required=True)
+    parser.add_argument("-t", "--title", default=None, type=lambda x: bytes(x, "utf-8").decode("unicode_escape"))
+    parser.add_argument("-y", "--ylim", default=None)
+    parser.add_argument("-l", "--ylabel", default=None, type=lambda x: bytes(x, "utf-8").decode("unicode_escape"))
+    args = parser.parse_args()
+    plot_external(
+        args.inputs,
+        args.output,
+        args.kernel_time,
+        args.config,
+        args.numerator,
+        args.denominator,
+        args.title,
+        args.ylim,
+        args.ylabel,
+    )
